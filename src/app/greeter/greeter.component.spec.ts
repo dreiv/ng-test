b21c26b2f5ce9c9a186f84e16541dbe21a8c0e2d
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-12-16 17:30:22 +0200

    created specs for fakeAsync / async, and also with a function call when done.

--- src/app/greeter/greeter.component.spec.ts
+++ src/app/greeter/greeter.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -11,6 +11,8 @@ describe('GreeterComponent', () => {
   let greeter: GreeterComponent;    // to access properties and methods
   let domElement: any;              // to access the DOM element
   let debugElement: DebugElement;   // testing helper
+
+  const correctText = 'Hello World!';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,12 +27,11 @@ describe('GreeterComponent', () => {
     debugElement = fixture.debugElement;
   });
 
-  it('should create', () => {
+  it('should create GreeterComponent', () => {
     expect(greeter).toBeTruthy();
   });
 
   it('should render `Hello World!` (async)', async(() => {
-    const correctText = 'Hello World!';
     greeter.name = 'World';
 
     fixture.detectChanges();
@@ -39,4 +40,25 @@ describe('GreeterComponent', () => {
       expect(debugElement.query(By.css('h2')).nativeElement.innerText).toBe(correctText);
     });
   }));
+
+  it('should render `Hello World!` (fakeAsync/tick)', fakeAsync(() => {
+    greeter.name = 'World';
+
+    fixture.detectChanges();
+    tick();   // execute all pending asynchronous calls
+    expect(domElement.querySelector('h2').innerText).toBe(correctText);
+    expect(debugElement.query(By.css('h2')).nativeElement.innerText).toBe(correctText);
+  }));
+
+  it('should render `Hello World!` (done)', done => {
+    greeter.name = 'World';
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(domElement.querySelector('h2').innerText).toBe(correctText);
+      expect(debugElement.query(By.css('h2')).nativeElement.innerText).toBe(correctText);
+      done();
+    });
+  })
+
 });
