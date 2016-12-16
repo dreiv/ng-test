dddac22611fd34fe76c68daa779f686998271c28
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-12-16 17:08:55 +0200

    managed to make karma more verbose, added a basic test that verifies component contents

--- src/app/greeter/greeter.component.spec.ts
+++ src/app/greeter/greeter.component.spec.ts
@@ -4,22 +4,39 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { GreeterComponent } from './greeter.component';
+import { element } from 'protractor';
 
 describe('GreeterComponent', () => {
-  let component: GreeterComponent;
+  let fixture: ComponentFixture<GreeterComponent>;
+  let greeter: GreeterComponent;    // to access properties and methods
+  let domElement: any;              // to access the DOM element
+  let debugElement: DebugElement;   // testing helper
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [GreeterComponent]
-    })
-      .compileComponents().then(() => {
-      const fixture = TestBed.createComponent(GreeterComponent);
-      component = fixture.componentInstance;
-      fixture.detectChanges();
-    });
+    }).compileComponents();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GreeterComponent);
+    greeter = fixture.componentInstance;
+    domElement = fixture.nativeElement;
+    debugElement = fixture.debugElement;
   });
+
+  it('should create', () => {
+    expect(greeter).toBeTruthy();
+  });
+
+  it('should render `Hello World!` (async)', async(() => {
+    const correctText = 'Hello World!';
+    greeter.name = 'World';
+
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(domElement.querySelector('h2').innerText).toBe(correctText);
+      expect(debugElement.query(By.css('h2')).nativeElement.innerText).toBe(correctText);
+    });
+  }));
 });
