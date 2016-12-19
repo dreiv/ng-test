c0fc7b521030c404817201314edb6ca1f83923cd
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-12-19 10:53:09 +0200

    mocked the http request with something angular makes available for testing.

--- src/app/languages/languages-http.service.spec.ts
+++ src/app/languages/languages-http.service.spec.ts
@@ -2,27 +2,39 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { LanguagesHttpService } from './languages-http.service';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Response, XHRBackend } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 describe('LanguagesHttpService', () => {
   let service: LanguagesHttpService;    // to access properties and methods
+  let mockBackend: any;                 // to mock Http backend responses
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpModule],
-      providers: [LanguagesHttpService]
+      providers: [
+        LanguagesHttpService,
+        {provide: XHRBackend, useClass: MockBackend}
+      ]
     });
   });
 
-  beforeEach(inject([LanguagesHttpService], s => {
-    service = s;
+  beforeEach(inject([LanguagesHttpService, XHRBackend], (_service, _mockBackend) => {
+    service = _service;
+    mockBackend = _mockBackend;
   }));
 
   it('should create the LanguagesHttpService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return the available languages', async(() => {
+  it('should return the available languages (mocked)', async(() => {
+    let response = ["en", "de", "fr"];
+
+    mockBackend.connections.subscribe(connection => {
+      connection.mockRespond(new Response({body: JSON.stringify(response)}));
+    });
+
     service.get().subscribe(languages => {
       expect(languages).toContain('en');
       expect(languages).toContain('de');
