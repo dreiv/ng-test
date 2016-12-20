b62e742c02a3d223b3d28a5c0e648ecd17c992c3
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-12-20 17:37:43 +0200

    at least I managed to test the hello service.

--- src/app/trial/hello.service.spec.ts
+++ src/app/trial/hello.service.spec.ts
@@ -2,15 +2,38 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { HelloService } from './hello.service';
+import { HttpModule, XHRBackend, ResponseOptions, Response } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
-describe('HelloService', () => {
+fdescribe('HelloService', () => {
+  let service: HelloService;
+  let mockBackend: any;       // to mock Http backend responses
+  let mockResponse: string = "hello";
+
+  // setup
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HelloService]
+      imports: [HttpModule],
+      providers: [HelloService, {provide: XHRBackend, useClass: MockBackend}]
     });
   });
 
-  it('should ...', inject([HelloService], (service: HelloService) => {
+  beforeEach(inject([HelloService, XHRBackend], (_service, _mockBackend) => {
+    service = _service;
+    mockBackend = _mockBackend;
+    mockBackend.connections.subscribe((connection: MockConnection) => {
+      connection.mockRespond(new Response(new ResponseOptions({body: JSON.stringify(mockResponse)})));
+    });
+  }))
+
+  // specs
+  it('should create an instance of HelloService', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('should return requested string (mocked)', () => {
+    service.get().subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+  });
 });
